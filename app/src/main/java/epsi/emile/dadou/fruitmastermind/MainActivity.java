32ec5eb2598432b885c0847c5735c5f5820acee8
YoYo2504 <yoan.filipe@espi.fr>
2019-12-23 17:28:58 +0100

    menu choice fruit user

--- app/src/main/java/epsi/emile/dadou/fruitmastermind/MainActivity.java
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/MainActivity.java
@@ -3,23 +3,102 @@ package epsi.emile.dadou.fruitmastermind;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnChoice1;
+    Button btnChoice2;
+    Button btnChoice3;
+    Button btnChoice4;
     private Button fruit1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        fruit1 = findViewById(R.id.button4);
+        fruit1 = findViewById(R.id.btnChoice1);
         fruit1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                  Toast.makeText(MainActivity.this, "Banane",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnChoice1 = (Button) findViewById(R.id.btnChoice1);
+        btnChoice2 = (Button) findViewById(R.id.btnChoice2);
+        btnChoice3 =(Button) findViewById(R.id.btnChoice3);
+        btnChoice4=(Button) findViewById(R.id.btnChoice4);
+
+        btnChoice1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, btnChoice1);
+                popup.getMenuInflater().inflate(R.menu.menu_choice_fruit, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        btnChoice2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, btnChoice2);
+                popup.getMenuInflater().inflate(R.menu.menu_choice_fruit, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        btnChoice3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, btnChoice3);
+                popup.getMenuInflater().inflate(R.menu.menu_choice_fruit, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();
+            }
+        });
+
+        btnChoice4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, btnChoice4);
+                popup.getMenuInflater().inflate(R.menu.menu_choice_fruit, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this,"You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+                    }
+                });
+
+                popup.show();
             }
         });
     }
