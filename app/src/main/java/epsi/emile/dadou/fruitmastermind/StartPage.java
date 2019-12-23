42c0455a2ac2052650c8233b714bda3b73645604
dadou97 <emile.dadou97@gmail.com>
2019-12-23 13:15:41 +0100

    Add first view and start design on second view

---
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/StartPage.java
@@ -0,0 +1,28 @@
+package epsi.emile.dadou.fruitmastermind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartPage extends AppCompatActivity {
+
+    private Button Play;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_page);
+        Play = findViewById(R.id.button1);
+        Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartPage.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
