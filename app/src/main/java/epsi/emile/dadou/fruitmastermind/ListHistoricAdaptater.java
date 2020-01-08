22faf1a6c2a4a6902acf62a4a5bffa7adefe2f27
YoYo2504 <yoan.filipe@espi.fr>
2020-01-08 23:44:56 +0100

    création class ListHistoric and ListHistoricAdaptater - YF

---
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/ListHistoricAdaptater.java
@@ -0,0 +1,23 @@
+package epsi.emile.dadou.fruitmastermind;
+
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListHistoricAdaptater extends ArrayAdapter<ListHistoric> {
+
+
+
+    public class ListHistoricHolder {
+        public ImageView image1;
+        public ImageView image2;
+        public ImageView image3;
+        public ImageView image4;
+
+        public TextView indice1;
+        public TextView indice2;
+        public TextView indice3;
+        public TextView indice4;
+    }
+
+}
