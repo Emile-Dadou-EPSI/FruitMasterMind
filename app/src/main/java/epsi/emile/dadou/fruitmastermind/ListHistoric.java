1d463a363220162fe91bc4441a0b49ad46c56331
YoYo2504 <yoan.filipe@espi.fr>
2020-01-09 16:33:28 +0100

    correction  YF

--- app/src/main/java/epsi/emile/dadou/fruitmastermind/ListHistoric.java
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/ListHistoric.java
@@ -2,21 +2,20 @@ package epsi.emile.dadou.fruitmastermind;
 
 
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.widget.ImageView;
 
 class ListHistoric {
-    private ImageView image1;
-    private ImageView image2;
-    private ImageView image3;
-    private ImageView image4;
+    private Drawable image1;
+    private Drawable image2;
+    private Drawable image3;
+    private Drawable image4;
 
     private String indice1;
     private String indice2;
     private String indice3;
     private String indice4;
 
-    public ListHistoric(ImageView image1, ImageView image2, ImageView image3, ImageView image4, String indice1, String indice2, String indice3, String indice4) {
+    public ListHistoric(Drawable image1, Drawable image2, Drawable image3, Drawable image4, String indice1, String indice2, String indice3, String indice4) {
         this.image1 = image1;
         this.image2 = image2;
         this.image3 = image3;
@@ -27,19 +26,19 @@ class ListHistoric {
         this.indice4 = indice4;
     }
 
-    public ImageView getImage1() {
+    public Drawable getImage1() {
         return image1;
     }
 
-    public ImageView getImage2() {
+    public Drawable getImage2() {
         return image2;
     }
 
-    public ImageView getImage3() {
+    public Drawable getImage3() {
         return image3;
     }
 
-    public ImageView getImage4() {
+    public Drawable getImage4() {
         return image4;
     }
 
@@ -59,19 +58,19 @@ class ListHistoric {
         return indice4;
     }
 
-    public void setImage1(ImageView image1) {
+    public void setImage1(Drawable image1) {
         this.image1 = image1;
     }
 
-    public void setImage2(ImageView image2) {
+    public void setImage2(Drawable image2) {
         this.image2 = image2;
     }
 
-    public void setImage3(ImageView image3) {
+    public void setImage3(Drawable image3) {
         this.image3 = image3;
     }
 
-    public void setImage4(ImageView image4) {
+    public void setImage4(Drawable image4) {
         this.image4 = image4;
     }
 
