0777f5c27a9953711098a11528d10762e39e6f7c
dadou97 <emile.dadou97@gmail.com>
2020-01-11 21:55:24 +0100

    the end is near

--- app/src/main/java/epsi/emile/dadou/fruitmastermind/MainActivity.java
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/MainActivity.java
@@ -1,24 +1,29 @@
 package epsi.emile.dadou.fruitmastermind;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.animation.TypeConverter;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.reflect.Array;
@@ -43,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
     List<ListHistoric> m_historic = new ArrayList<ListHistoric>() ;
 
     int MAX_FRUITS = 4;
+    int COUPS_RESTANTS = 10;
+    TextView NbTries;
+    int tag_hints = 0;
 
    // Listes des boutons pour choisir les fruits
     public Button btnChoice1;
@@ -75,7 +83,19 @@ public class MainActivity extends AppCompatActivity {
     private List<Fruit> listeTest2 = new ArrayList<>();
 
 
-
+    private LinearLayout indices;
+    private TextView indicePep;
+    private TextView indiceEp;
+    private LinearLayout indicePepin;
+    private LinearLayout indicesEpluchure;
+    private TextView indPep1;
+    private TextView indPep2;
+    private TextView indPep3;
+    private TextView indPep4;
+    private TextView indEp1;
+    private TextView indEp2;
+    private TextView indEp3;
+    private TextView indEp4;
 
 
 
@@ -86,8 +106,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list = findViewById(R.id.recyclerview);
+        NbTries = findViewById(R.id.triesNb);
         listToFind = initGameList();
-        initTestListe(listeTest1, listeTest2);
         Toast.makeText(MainActivity.this, listToFind.get(0).getName() + listToFind.get(1).getName() + listToFind.get(2).getName() + listToFind.get(3).getName(), Toast.LENGTH_LONG).show();
         //Buttons for User Choice
         btnChoice1 = (Button) findViewById(R.id.btnChoice1);
@@ -416,15 +436,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Toast.makeText(MainActivity.this, "" + userGuessList.get(0).getName(), Toast.LENGTH_LONG).show();
                 //Toast.makeText(MainActivity.this, userGuessList.get(0).getName() + userGuessList.get(1).getName() + userGuessList.get(2).getName() + userGuessList.get(3).getName(), Toast.LENGTH_LONG).show();
-                Toast.makeText(MainActivity.this, listToFind.get(0).getName() + listToFind.get(1).getName() + listToFind.get(2).getName() + listToFind.get(3).getName(), Toast.LENGTH_LONG).show();
+                //Toast.makeText(MainActivity.this, listToFind.get(0).getName() + listToFind.get(1).getName() + listToFind.get(2).getName() + listToFind.get(3).getName(), Toast.LENGTH_LONG).show();
                 Resources res = getResources();
                 charToInsert = checkUserInput(userGuessList, listToFind, MAX_FRUITS);
-                checkVictoryConditions(listToFind, userGuessList);
+                Boolean endGame = checkVictoryConditions(charToInsert);
                 adapter.addListHisto(new ListHistoric(res, tabIntFruits, charToInsert));
                 list.setLayoutManager(lm);
                 list.setAdapter(adapter);
-                userGuessList.clear();
-                charToInsert.clear();
+                COUPS_RESTANTS = COUPS_RESTANTS - 1;
+                NbTries.setText(String.valueOf(COUPS_RESTANTS));
+                Toast.makeText(MainActivity.this, String.valueOf(endGame), Toast.LENGTH_LONG).show();
+                if (endGame.booleanValue() == true) {
+                    popupEndGame();
+                    Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
+                }
+                else if (endGame.booleanValue() == false && COUPS_RESTANTS == 0) {
+                    popupEndGame();
+                    Toast.makeText(MainActivity.this, "Fail", Toast.LENGTH_LONG).show();
+                }
+
+                //userGuessList.clear();
+                //charToInsert.clear();
 
                 //list.notify();
             }
@@ -474,12 +506,12 @@ public class MainActivity extends AppCompatActivity {
 
     // Vérification de victoire si les deux listes sont égales
     // useless
-    private void checkVictoryConditions(List<Fruit> listToGuess, List<Fruit> userGuessList) {
-        if (listToGuess.equals(userGuessList)) {
-            //Toast.makeText(MainActivity.this, "Yay you're the master fruiter",Toast.LENGTH_SHORT).show();
+    private Boolean checkVictoryConditions(List<Character> result) {
+        if (result.get(0) == 'X' && result.get(1) == 'X' && result.get(2) == 'X' && result.get(3) == 'X') {
+            return true;
         }
         else {
-            //Toast.makeText(MainActivity.this,"Fail", Toast.LENGTH_LONG).show();
+            return false;
         }
     }
 
@@ -493,33 +525,160 @@ public class MainActivity extends AppCompatActivity {
         //Toast.makeText(MainActivity.this, String.valueOf(userGuessList.size()), Toast.LENGTH_LONG).show();
         for(int i = 0; i < MAX_FRUITS; i++){
             char tmpResult = 'O';
-            result.add(i, tmpResult);
+            //result.add(i, tmpResult);
 
             for(int j=0; j < MAX_FRUITS; j++){
-                if(userInput.get(i).getName() == inGameFruits.get(i).getName()){
+                if(userInput.get(i).getName().equals(inGameFruits.get(j).getName())){
                     tmpResult = 'V';
-                    result.add(i, tmpResult);
+                    //result.add(i, tmpResult);
                     if(i == j){
                         tmpResult = 'X';
-                        result.add(i, tmpResult);
                     }
                 }
             }
+            result.add(i, tmpResult);
         }
+
         //Toast.makeText(MainActivity.this, result.get(0) + result.get(1) + result.get(2) + result.get(3), Toast.LENGTH_LONG).show();
+
         return result;
     }
 
-    private void initTestListe(List<Fruit> test1, List<Fruit> test2) {
-        test1.add(0, new Fruit(enumFruits.BANANE));
-        test1.add(1, new Fruit(enumFruits.BANANE));
-        test1.add(2, new Fruit(enumFruits.BANANE));
-        test1.add(3, new Fruit(enumFruits.KIWI));
 
-        test2.add(0, new Fruit(enumFruits.BANANE));
-        test2.add(1, new Fruit(enumFruits.BANANE));
-        test2.add(2, new Fruit(enumFruits.BANANE));
-        test2.add(3, new Fruit(enumFruits.BANANE));
+
+
+    //==========================================
+    //==========================================
+    // Partie de Yoan
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_indice, menu);
+        return true;
+    }
+
+    // afficher indice :
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        indices = findViewById(R.id.indices);
+        indices.setVisibility(View.VISIBLE);
+        switch (item.getItemId()){
+            case R.id.indice1:
+                afficherIndice1();
+                useIndice();
+                Toast.makeText(this,item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.indice2:
+                afficherIndice2();
+                useIndice();
+                Toast.makeText(this,item.getTitle(), Toast.LENGTH_LONG).show();
+                break;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        return true;
+    }
+
+    public void afficherIndice1(){
+
+        indicePep = findViewById(R.id.indicePepinTxt);
+        indicePepin = findViewById(R.id.indicePepin);
+        indPep1 = findViewById(R.id.indicePepin1);
+        indPep2 = findViewById(R.id.indicePepin2);
+        indPep3 = findViewById(R.id.indicePepin3);
+        indPep4 = findViewById(R.id.indicePepin4);
+
+        indicePep.setVisibility(View.VISIBLE);
+        indicePepin.setVisibility(View.VISIBLE);
+        indPep1.setVisibility(View.VISIBLE);
+        indPep2.setVisibility(View.VISIBLE);
+        indPep3.setVisibility(View.VISIBLE);
+        indPep4.setVisibility(View.VISIBLE);
+
+    }
+
+    public void afficherIndice2(){
+
+        indiceEp = findViewById(R.id.indiceEpTxt);
+        indicesEpluchure = findViewById(R.id.indiceEp);
+        indEp1 = findViewById(R.id.indiceEp1);
+        indEp2 = findViewById(R.id.indiceEp2);
+        indEp3 = findViewById(R.id.indiceEp3);
+        indEp4 = findViewById(R.id.indiceEp4);
+
+        indiceEp.setVisibility(View.VISIBLE);
+        indicesEpluchure.setVisibility(View.VISIBLE);
+        indEp1.setVisibility(View.VISIBLE);
+        indEp2.setVisibility(View.VISIBLE);
+        indEp3.setVisibility(View.VISIBLE);
+        indEp4.setVisibility(View.VISIBLE);
+
+    }
+
+    public void useIndice(){
+        if (tag_hints == 0 && COUPS_RESTANTS > 2) {
+            COUPS_RESTANTS = COUPS_RESTANTS - 2;
+            NbTries.setText(String.valueOf(COUPS_RESTANTS));
+            tag_hints = 1;
+        }
+        else if (tag_hints == 1 && COUPS_RESTANTS > 3) {
+            COUPS_RESTANTS = COUPS_RESTANTS - 3;
+            NbTries.setText(String.valueOf(COUPS_RESTANTS));
+            tag_hints = 2;
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Vous avez utilisez tout les indices disponibles", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+    private void popupEndGame(){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        View mView = getLayoutInflater().inflate(R.layout.activity_end_game, null);
+        TextView mVictoireDefaite = (TextView) mView.findViewById(R.id.VictoireDefaite);
+        Button mNewGame = (Button) mView.findViewById(R.id.NewGame);
+        Button mRestart = (Button) mView.findViewById(R.id.Restart);
+        Button mQuitt = (Button) mView.findViewById(R.id.Quitt);
+
+        mBuilder.setView(mView);
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+
+        mNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+
+        mRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                COUPS_RESTANTS = 10;
+                tag_hints = 0;
+                adapter.clearList();
+                NbTries.setText(String.valueOf(COUPS_RESTANTS));
+                dialog.cancel();
+            }
+        });
+
+        mQuitt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openStartPage();
+            }
+        });
+
+
+    }
+
+    public void openStartPage(){
+        Intent intent = new Intent(this, StartPage.class);
+        startActivity(intent);
     }
 
 }
