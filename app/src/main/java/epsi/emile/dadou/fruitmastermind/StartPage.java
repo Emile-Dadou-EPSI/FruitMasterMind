aeff8e55f48d1c972007676e01b6b27096e5e687
GuillaumeGouhier <guillaume.gouhier1999@gmail.com>
2020-01-11 11:50:56 +0100

    Add Comments

--- app/src/main/java/epsi/emile/dadou/fruitmastermind/StartPage.java
+++ app/src/main/java/epsi/emile/dadou/fruitmastermind/StartPage.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+//Landing Page / Activity
 public class StartPage extends AppCompatActivity {
 
     private Button Play;
